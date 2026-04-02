4f7ae2ca1cd733c41f42bafbfd1388dd45910799
RobbeGyselbrecht <robbe.gyselbrecht@gmail.com>
2026-04-02 15:14:50 +0200

    3 buttons extra

--- Software/testRPI/Nieuweknoppen.py
+++ Software/testRPI/Nieuweknoppen.py
@@ -11,13 +11,21 @@ TOPIC_BATTERY = "cockpit/input/battery"
 TOPIC_TOGGLE = "cockpit/input/carb-heat"   # <-- aanpassen
 TOPIC_EXTRA = "cockpit/input/alt"          # <-- alt
 TOPIC_SLEUTEL = "cockpit/input/sleutel"    # <-- nieuw
-TOPIC_PRIMER = "cockpit/input/primer-lever"
+TOPIC_PRIMER = "cockpit/input/primer"
+
+TOPIC_SWITCH_1 = "cockpit/input/switch1"
+TOPIC_SWITCH_2 = "cockpit/input/switch2"
+TOPIC_SWITCH_3 = "cockpit/input/switch3"
 
 BUTTON_PIN = 17
 TOGGLE_BUTTON_PIN = 27
 EXTRA_BUTTON_PIN = 22
-SLEUTEL_PIN = 23                           # <-- aanpassen indien nodig
+SLEUTEL_PIN = 23
 PRIMER_PIN = 18
+
+SWITCH_1_PIN = 24
+SWITCH_2_PIN = 25
+SWITCH_3_PIN = 5
 
 # ========================
 # GPIO SETUP (lgpio)
@@ -39,6 +47,11 @@ lgpio.gpio_claim_input(chip, SLEUTEL_PIN, lgpio.SET_PULL_UP)
 # Primer knop
 lgpio.gpio_claim_input(chip, PRIMER_PIN, lgpio.SET_PULL_UP)
 
+# Extra schakelaars
+lgpio.gpio_claim_input(chip, SWITCH_1_PIN, lgpio.SET_PULL_UP)
+lgpio.gpio_claim_input(chip, SWITCH_2_PIN, lgpio.SET_PULL_UP)
+lgpio.gpio_claim_input(chip, SWITCH_3_PIN, lgpio.SET_PULL_UP)
+
 # ========================
 # MQTT SETUP
 # ========================
@@ -46,7 +59,7 @@ client = mqtt.Client()
 client.connect(BROKER, 1883, 60)
 client.loop_start()
 
-print("Battery + toggle + extra + sleutel + primer input started (lgpio)...")
+print("Battery + toggle + extra + sleutel + primer + 3 switches input started (lgpio)...")
 
 # ========================
 # STATES
@@ -58,6 +71,10 @@ last_sleutel_state = None
 
 primer_state = 0
 last_primer_gpio = 1
+
+last_switch_1_state = None
+last_switch_2_state = None
+last_switch_3_state = None
 
 try:
     while True:
@@ -117,6 +134,39 @@ try:
             time.sleep(0.2)  # debounce
 
         last_primer_gpio = current_primer_gpio
+
+        # ========================
+        # 6) SWITCH 1 (schakelaar)
+        # ========================
+        current_switch_1_gpio = lgpio.gpio_read(chip, SWITCH_1_PIN)
+        switch_1_state = 1 if current_switch_1_gpio == 1 else 0
+
+        if switch_1_state != last_switch_1_state:
+            client.publish(TOPIC_SWITCH_1, str(switch_1_state))
+            print("Switch 1:", switch_1_state)
+            last_switch_1_state = switch_1_state
+
+        # ========================
+        # 7) SWITCH 2 (schakelaar)
+        # ========================
+        current_switch_2_gpio = lgpio.gpio_read(chip, SWITCH_2_PIN)
+        switch_2_state = 1 if current_switch_2_gpio == 1 else 0
+
+        if switch_2_state != last_switch_2_state:
+            client.publish(TOPIC_SWITCH_2, str(switch_2_state))
+            print("Switch 2:", switch_2_state)
+            last_switch_2_state = switch_2_state
+
+        # ========================
+        # 8) SWITCH 3 (schakelaar)
+        # ========================
+        current_switch_3_gpio = lgpio.gpio_read(chip, SWITCH_3_PIN)
+        switch_3_state = 1 if current_switch_3_gpio == 1 else 0
+
+        if switch_3_state != last_switch_3_state:
+            client.publish(TOPIC_SWITCH_3, str(switch_3_state))
+            print("Switch 3:", switch_3_state)
+            last_switch_3_state = switch_3_state
 
         time.sleep(0.05)
 
