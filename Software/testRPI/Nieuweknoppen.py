e07781dbe870d3683460b640b9a337aa0b3aea9e
RobbeGyselbrecht <robbe.gyselbrecht@gmail.com>
2026-04-02 14:03:56 +0200

    Primer

--- Software/testRPI/Nieuweknoppen.py
+++ Software/testRPI/Nieuweknoppen.py
@@ -9,13 +9,15 @@ BROKER = "localhost"
 
 TOPIC_BATTERY = "cockpit/input/battery"
 TOPIC_TOGGLE = "cockpit/input/carb-heat"   # <-- aanpassen
-TOPIC_EXTRA = "cockpit/input/alt"                 # <-- alt
-TOPIC_SLEUTEL = "cockpit/input/sleutel"           # <-- nieuw
+TOPIC_EXTRA = "cockpit/input/alt"          # <-- alt
+TOPIC_SLEUTEL = "cockpit/input/sleutel"    # <-- nieuw
+TOPIC_PRIMER = "cockpit/input/primer"
 
 BUTTON_PIN = 17
 TOGGLE_BUTTON_PIN = 27
 EXTRA_BUTTON_PIN = 22
-SLEUTEL_PIN = 23                                  # <-- aanpassen indien nodig
+SLEUTEL_PIN = 23                           # <-- aanpassen indien nodig
+PRIMER_PIN = 18
 
 # ========================
 # GPIO SETUP (lgpio)
@@ -34,6 +36,9 @@ lgpio.gpio_claim_input(chip, EXTRA_BUTTON_PIN, lgpio.SET_PULL_UP)
 # Sleutel knop (momentary)
 lgpio.gpio_claim_input(chip, SLEUTEL_PIN, lgpio.SET_PULL_UP)
 
+# Primer knop
+lgpio.gpio_claim_input(chip, PRIMER_PIN, lgpio.SET_PULL_UP)
+
 # ========================
 # MQTT SETUP
 # ========================
@@ -41,7 +46,7 @@ client = mqtt.Client()
 client.connect(BROKER, 1883, 60)
 client.loop_start()
 
-print("Battery + toggle + extra + sleutel input started (lgpio)...")
+print("Battery + toggle + extra + sleutel + primer input started (lgpio)...")
 
 # ========================
 # STATES
@@ -50,6 +55,9 @@ last_battery_state = None
 last_toggle_state = None
 last_extra_state = None
 last_sleutel_state = None
+
+primer_state = 0
+last_primer_gpio = 1
 
 try:
     while True:
@@ -96,6 +104,19 @@ try:
             client.publish(TOPIC_SLEUTEL, str(sleutel_state))
             print("Sleutel:", sleutel_state)
             last_sleutel_state = sleutel_state
+
+        # ========================
+        # 5) PRIMER (toggle bij elke druk)
+        # ========================
+        current_primer_gpio = lgpio.gpio_read(chip, PRIMER_PIN)
+
+        if current_primer_gpio == 0 and last_primer_gpio == 1:
+            primer_state = 0 if primer_state == 1 else 1
+            client.publish(TOPIC_PRIMER, str(primer_state))
+            print("Primer:", primer_state)
+            time.sleep(0.2)  # debounce
+
+        last_primer_gpio = current_primer_gpio
 
         time.sleep(0.05)
 
