7abbe4bfa8fb40018df4e999bbdd5abef664b837
RobbeGyselbrecht <robbe.gyselbrecht@gmail.com>
2026-04-02 14:17:09 +0200

    name change primer

--- Software/testRPI/Nieuweknoppen.py
+++ Software/testRPI/Nieuweknoppen.py
@@ -11,7 +11,7 @@ TOPIC_BATTERY = "cockpit/input/battery"
 TOPIC_TOGGLE = "cockpit/input/carb-heat"   # <-- aanpassen
 TOPIC_EXTRA = "cockpit/input/alt"          # <-- alt
 TOPIC_SLEUTEL = "cockpit/input/sleutel"    # <-- nieuw
-TOPIC_PRIMER = "cockpit/input/primer"
+TOPIC_PRIMER = "cockpit/input/primer-lever"
 
 BUTTON_PIN = 17
 TOGGLE_BUTTON_PIN = 27
